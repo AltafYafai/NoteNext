5aee89a7b3a2054506f62fdb52af63daa0eca21f
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2026-03-21 08:11:23 +0530

    fix: add missing FilterByProject branch to EventConverter

--- app/src/main/java/com/suvojeet/notenext/ui/project/EventConverter.kt
+++ app/src/main/java/com/suvojeet/notenext/ui/project/EventConverter.kt
@@ -60,6 +60,7 @@ fun NotesEvent.toProjectNotesEvent(): ProjectNotesEvent {
         is NotesEvent.NavigateToNoteByTitle -> ProjectNotesEvent.NavigateToNoteByTitle(this.title)
         is NotesEvent.CreateProject -> throw IllegalArgumentException("CreateProject event cannot be converted")
         is NotesEvent.FilterByLabel -> throw IllegalArgumentException("FilterByLabel event cannot be converted")
+        is NotesEvent.FilterByProject -> throw IllegalArgumentException("FilterByProject event cannot be converted")
         is NotesEvent.OnReminderChange -> ProjectNotesEvent.OnReminderChange(this.time, this.repeatOption)
         is NotesEvent.MoveSelectedNotesToProject -> throw IllegalArgumentException("MoveSelectedNotesToProject event cannot be converted")
         is NotesEvent.ToggleLockForSelectedNotes -> ProjectNotesEvent.ToggleLockForSelectedNotes
